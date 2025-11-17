166e295bb31aff0d9c2e1c2487ed9f33574ede2d
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-11-17 12:34:53 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
+import { Link } from "react-router-dom"
+
+
 export default function Navbar() {
 
     return (
 
-        <>
-            Navbar
-        </>
+        <nav className="navbar navbar-light bg-light">
+            <ul className="nav nav-pills">
+                <li className="navbar-item">
+                    <Link to='/' className="nav-link active">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to='/chi-siamo' className="nav-link">Chi Siamo</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to='/prodotti' className="nav-link">Prodotti</Link>
+                </li>
+            </ul>
+        </nav>
 
     )
 
